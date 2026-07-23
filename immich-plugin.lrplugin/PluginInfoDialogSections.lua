558c5b16f37f52cb1711b861239154c754420e22
fjb2020 <92430217+fjb2020@users.noreply.github.com>
2026-07-23 14:26:23 +0100

    Add option to log to console

--- immich-plugin.lrplugin/PluginInfoDialogSections.lua
+++ immich-plugin.lrplugin/PluginInfoDialogSections.lua
@@ -7,6 +7,10 @@ function PluginInfoDialogSections.startDialog(propertyTable)
         prefs.logging = false
     end
     propertyTable.logging = prefs.logging
+    if prefs.logtofile == nil then
+        prefs.logtofile = false
+    end
+    propertyTable.logtofile = prefs.logtofile
 end
 
 function PluginInfoDialogSections.sectionsForBottomOfDialog(f, propertyTable)
@@ -28,6 +32,14 @@ function PluginInfoDialogSections.sectionsForBottomOfDialog(f, propertyTable)
                 f:checkbox({
                     title = "Enable debug logging",
                     value = bind("logging"),
+                }),
+
+            }),
+            f:row({
+                spacing = f:control_spacing(),
+                f:checkbox({
+                    title = "Log to file (otherwise log to console)",
+                    value = bind("logtofile"),
                 }),
                 f:push_button({
                     title = "Show logfile",
@@ -59,6 +71,11 @@ function PluginInfoDialogSections.endDialog(propertyTable)
     prefs.logging = propertyTable.logging
     if propertyTable.logging then
         log:enable("logfile")
+        if propertyTable.logtofile then
+            log:enable("logfile")
+        else
+            log:enable("print")
+        end
     else
         log:disable()
     end
